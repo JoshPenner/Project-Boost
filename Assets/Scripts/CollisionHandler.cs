4903fc579f205712c2cc166c424b6125ad7b700d
JoshPenner <ircobra@gmail.com>
2021-02-22 10:52:17 -0800

    Initial commit
    Added all files

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("this is friendly");
+                break;
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex+1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-Boost
+ GameDev Unity 3D course. Fly a spaceship through a hostile environment.
